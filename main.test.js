c0780052b743b48cc4a889e10e266cbf6b7001f6
Sefina-DS <ds.sefina@gmail.com>
2026-06-17 16:44:21 +0200

    fix: repair test suite, remove invalid should() syntax

--- main.test.js
+++ main.test.js
@@ -1,29 +1,10 @@
 'use strict';
 
-/**
- * This is a dummy TypeScript test file using chai and mocha
- *
- * It's automatically excluded from npm and its build output is excluded from both git and npm.
- * It is advised to test all your modules with accompanying *.test.js-files
- */
-
-// tslint:disable:no-unused-expression
-
 const { expect } = require('chai');
-// import { functionToTest } from "./moduleToTest";
 
-describe('module to test => function to test', () => {
-  // initializing logic
-  const expected = 5;
-
-  it(`should return ${expected}`, () => {
+describe('Dreame Adapter — basic tests', () => {
+  it('expect syntax works', () => {
     const result = 5;
-    // assign result a value from functionToTest
-    expect(result).to.equal(expected);
-    // or using the should() syntax
-    result.should.equal(expected);
+    expect(result).to.equal(5);
   });
-  // ... more tests => it
 });
-
-// ... more test suites => describe
